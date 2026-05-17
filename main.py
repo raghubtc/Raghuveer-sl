9e2fa0e2ec551279cfb73bad373939d264b742f6
raghubtc <40029282+raghubtc@users.noreply.github.com>
2026-05-17 08:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,8 +121,7 @@ trailing_stop_active = False
 
 @app.route('/')
 def index():
-    @app.route('/')
-def index():
+    return render_template('index.html')
     return render_template('index.html')
 
 @app.route('/health')
